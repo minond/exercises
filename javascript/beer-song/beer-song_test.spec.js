f9148e0f0ef11b12ad60aff5b26511d5f8a5e74d
Marcos Minond <minond.marcos@gmail.com>
2014-05-05 22:42:15 -0600

    running last test on beer-song js

--- javascript/beer-song/beer-song_test.spec.js
+++ javascript/beer-song/beer-song_test.spec.js
@@ -17,7 +17,7 @@ describe('Beer', function() {
     expect(Beer.verse(0)).toEqual(expected);
   });
 
-  xit('sings several verses', function() {
+  it('sings several verses', function() {
     var expected = "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n\n7 bottles of beer on the wall, 7 bottles of beer.\nTake one down and pass it around, 6 bottles of beer on the wall.\n\n6 bottles of beer on the wall, 6 bottles of beer.\nTake one down and pass it around, 5 bottles of beer on the wall.\n";
     expect(Beer.sing(8, 6)).toEqual(expected);
   });
