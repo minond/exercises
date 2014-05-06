d3af739c50a3e8f4d62bfc5567767ea29e5bea4a
Marcos Minond <minond.marcos@gmail.com>
2014-05-05 20:21:11 -0600

    fixing bob js

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -6,13 +6,13 @@ function Bob () {}
 Bob.prototype.hey = function (message) {
     'use strict';
 
+    message = message || '';
+
     var response = 'Whatever.',
         has_letter = /[a-zA-Z]/.test(message),
         is_empty = !message.replace(/\s+/, ''),
         is_question = message.substr(-1) === '?',
         is_upper = message === message.toUpperCase();
-
-    message = message || '';
 
     if (is_empty) {
         response = 'Fine. Be that way!';
