ab4ddbf9c206f711944dabe6952f8e6c08a78faa
Marcos Minond <minond.marcos@gmail.com>
2014-12-29 16:05:44 -0700

    python bob

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,6 +1,21 @@
-#
-# Skeleton file for the Python "Bob" exercise.
-#
-def hey(what):
+import re
 
-    return
+def is_question(message):
+    return message.endswith('?')
+
+def is_yelling(message):
+    return message.upper() == message and re.search('[a-zA-Z]+', message)
+
+def is_nothing(message):
+    return not message or message.strip() == ''
+
+class Bob:
+    def hey(self, message):
+        if is_yelling(message):
+            return 'Woah, chill out!'
+        elif is_question(message):
+            return 'Sure.'
+        elif is_nothing(message):
+            return 'Fine. Be that way!'
+        else:
+            return 'Whatever.'
