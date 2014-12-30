67ba7968668ecf2ae0126fc71023b15ced062502
Marcos Minond <minond.marcos@gmail.com>
2014-12-30 11:13:15 -0700

    smaller

--- python/leap/year.py
+++ python/leap/year.py
@@ -2,11 +2,5 @@ def is_divisible_by(numerator, denominator):
     return numerator % denominator == 0
 
 def is_leap_year(year):
-    if is_divisible_by(year, 400):
-        return True
-    elif is_divisible_by(year, 100):
-        return False
-    elif is_divisible_by(year, 4):
-        return True
-    else:
-        return False
+    return is_divisible_by(year, 4) and (not is_divisible_by(year, 100) \
+        or is_divisible_by(year, 400))
