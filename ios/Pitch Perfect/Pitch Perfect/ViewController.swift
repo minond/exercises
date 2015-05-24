6ed091daa4c73ba5f7c03e7c0c978b2c5bc44721
Marcos Minond <minond.marcos@gmail.com>
2015-05-24 15:03:08 -0600

    recording label outlet

--- ios/Pitch Perfect/Pitch Perfect/ViewController.swift
+++ ios/Pitch Perfect/Pitch Perfect/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var recordingInProgress: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
