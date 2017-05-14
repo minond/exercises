c5d378b7ea7498ac8cdae9dde4611c33fedc90cb
Marcos Minond <minond.marcos@gmail.com>
2017-05-14 13:27:44 -0600

    refactoring seconds on earth prog

--- scala/space-age/src/main/scala/SpaceAge.scala
+++ scala/space-age/src/main/scala/SpaceAge.scala
@@ -1,6 +1,4 @@
-import scala.math.BigDecimal
-
-object SpaceAge {
+case class SpaceAge (val seconds: Long) {
   val SECONDS_ON_EARTH = 31557600
   val MERCURY_RATIO = 0.2408467
   val VENUS_RATIO = 0.61519726
@@ -10,26 +8,17 @@ object SpaceAge {
   val URANUS_RATIO = 84.016846
   val NEPTUNE_RATIO = 164.79132
 
-  case class Age(seconds: Long, onEarth: Double, onMercury: Double,
-    onVenus: Double, onMars: Double, onJupiter: Double, onSaturn: Double,
-    onUranus: Double, onNeptune: Double)
+  lazy val EARTH_AGE = seconds.toDouble / SECONDS_ON_EARTH
 
-  def apply(sec: Long): Age = {
-    val onEarth = BigDecimal(sec.toDouble / SECONDS_ON_EARTH)
+  private def round(dec: Double) =
+    Math.round(dec * 100).toDouble / 100.0
 
-    Age(
-      seconds = sec,
-      onEarth = toDouble(onEarth),
-      onMercury = toDouble(onEarth / MERCURY_RATIO),
-      onVenus = toDouble(onEarth / VENUS_RATIO),
-      onMars = toDouble(onEarth / MARS_RATIO),
-      onJupiter = toDouble(onEarth / JUPITER_RATIO),
-      onSaturn = toDouble(onEarth / SATURN_RATIO),
-      onUranus = toDouble(onEarth / URANUS_RATIO),
-      onNeptune = toDouble(onEarth / NEPTUNE_RATIO)
-    )
-  }
-
-  private def toDouble(dec: BigDecimal): Double =
-    dec.setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
+  def onEarth = round(EARTH_AGE)
+  def onMercury = round(EARTH_AGE / MERCURY_RATIO)
+  def onVenus = round(EARTH_AGE / VENUS_RATIO)
+  def onMars = round(EARTH_AGE / MARS_RATIO)
+  def onJupiter = round(EARTH_AGE / JUPITER_RATIO)
+  def onSaturn = round(EARTH_AGE / SATURN_RATIO)
+  def onUranus = round(EARTH_AGE / URANUS_RATIO)
+  def onNeptune = round(EARTH_AGE / NEPTUNE_RATIO)
 }
