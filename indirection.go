4a8e465918b00773e130dc60ad6c2173c50490b6
Marcos Minond <minond.marcos@gmail.com>
2017-10-07 16:46:12 -0600

    1 - 6

---
+++ indirection.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v *Vertex) Scale(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+func ScaleFunc(v *Vertex, f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+func main() {
+	v := Vertex{3, 4}
+	v.Scale(2)
+	ScaleFunc(&v, 10)
+
+	p := &Vertex{3, 4}
+	p.Scale(2)
+	ScaleFunc(p, 10)
+
+	fmt.Println(v, p)
+}
