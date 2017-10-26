5c9c27be8d2ff7920efc9f59210dfaf439577aac
Marcos Minond <minond.marcos@gmail.com>
2017-10-25 20:33:17 -0600

    7 - 8

---
+++ mutex-counter.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type SafeCounter struct {
+	v   map[string]int
+	mux sync.Mutex
+}
+
+func (c *SafeCounter) Inc(key string) {
+	c.mux.Lock()
+	c.v[key]++
+	c.mux.Unlock()
+}
+
+func (c *SafeCounter) Value(key string) int {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	return c.v[key]
+}
+
+func main() {
+	c := SafeCounter{v: make(map[string]int)}
+
+	for i := 0; i < 1000; i++ {
+		go c.Inc("mykey")
+	}
+
+	time.Sleep(time.Second)
+	fmt.Println(c.Value("mykey"))
+}
