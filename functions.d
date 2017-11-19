29d8a3ab368805bc7c50732c694dfd267401a1bd
Marcos Minond <minond.marcos@gmail.com>
2017-11-18 22:09:32 -0700

    more function samples

--- functions.d
+++ functions.d
@@ -1,6 +1,11 @@
 import std.stdio : writeln;
 import std.random : uniform;
 
+static assert(!__traits(compiles, add(1, 2)));
+static assert(!__traits(compiles, sub(1, 2)));
+static assert(!__traits(compiles, mul(1, 2)));
+static assert(!__traits(compiles, div(1, 2)));
+
 // Functions can be delcared as local functions, which have lexical scope.
 // Nested functions are called delegates in D.
 void main() {
@@ -14,4 +19,48 @@ void main() {
 
   greeting("Hello");
   greeting("Hello", "Marcos");
+
+  auto add(int lhs, int rhs) {
+    return lhs + rhs;
+  }
+
+  auto sub(int lhs, int rhs) {
+    return lhs - rhs;
+  }
+
+  auto mul(int lhs, int rhs) {
+    return lhs * rhs;
+  }
+
+  auto div(int lhs, int rhs) {
+    return lhs / rhs;
+  }
+
+  int a = 27;
+  int b = 42;
+
+  switch (uniform(0, 4)) {
+  case 0:
+    writeln(add(a, b));
+    break;
+
+  case 1:
+    writeln(sub(a, b));
+    break;
+
+  case 2:
+    writeln(mul(a, b));
+    break;
+
+  case 3:
+    writeln(div(a, b));
+    break;
+
+  default: assert(0);
+  }
+
+  static assert(__traits(compiles, add(1, 2)));
+  static assert(__traits(compiles, sub(1, 2)));
+  static assert(__traits(compiles, mul(1, 2)));
+  static assert(__traits(compiles, div(1, 2)));
 }
