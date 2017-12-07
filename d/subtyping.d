260546d7384cdf92a7cfaa9083f6149a935b2840
Marcos Minond <minond.marcos@gmail.com>
2017-12-07 14:57:54 -0700

    subtyping.d

--- d/subtyping.d
+++ d/subtyping.d
@@ -2,7 +2,12 @@ import std.stdio : writeln;
 
 struct Vector3 {
   private double[3] vec;
-  alias vec this;
+
+  // Any function or on Vector3 that can't be handled by Vector3 will be passed
+  // on to double[3]. This let's us treat Vector3 like any other double[3],
+  // like assigning a double[3] to a Vector3 and using any other
+  // property/method that works with double[]/double[3].
+  lalias vec this;
 
   double dot(Vector3 rhs) {
     return vec[0] * rhs.vec[0] + vec[1] * rhs.vec[1] + vec[2] * rhs.vec[2];
