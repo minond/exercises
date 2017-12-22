fe660bcbf65d6777b27bb4280788b6fabc3c297b
Marcos Minond <minond.marcos@gmail.com>
2017-12-22 15:10:48 -0700

    q5

--- Programming Languages/Programming Languages B/Week 1/hw4.rkt
+++ Programming Languages/Programming Languages B/Week 1/hw4.rkt
@@ -59,3 +59,14 @@
                   (cond [(= i n) null]
                         [#t (cons (nth i s) (next (+ i 1)))]))])
     (next 0)))
+
+#| 5. Write a stream funny-number-stream that is like the stream of natural
+   numbers (i.e., 1, 2, 3, ...) except numbers divisble by 5 are negated
+   (i.e., 1, 2, 3, 4, -5, 6, 7, 8, 9, -10, 11, ...). Remember a stream is a
+   thunk that when called produces a pair. Here the car of the pair will be a
+   number and the cdr will be another stream. |#
+(define funny-number-stream
+  (letrec ([f (lambda (n)
+                (cons (cond [(zero? (modulo n 5)) (- n)]
+                            [#t n]) (lambda () (f (+ n 1)))))])
+    (lambda () (f 1))))
