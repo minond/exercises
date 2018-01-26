60ab81317c0debff9f4c2225c018ad0143426c68
Marcos Minond <minond.marcos@gmail.com>
2018-01-26 16:27:45 -0700

    object_state.rb

--- Programming Languages C/Week 1/object_state.rb
+++ Programming Languages C/Week 1/object_state.rb
@@ -1,19 +1,44 @@
 class A
+  ONE = 1
+
+  @@global_increment = 0
+
   def initialize (f = 0)
-    @foo = f
+    @local_increment = f
   end
 
-  def foo
-    @foo
+  def local_increment
+    @local_increment
+  end
+
+  def global_increment
+    @@global_increment
+  end
+
+  def self.global_increment
+    @@global_increment
   end
 
   def increment
-    @foo += 1
+    @local_increment += ONE
+    @@global_increment += ONE
   end
 end
 
-a = A.new 10
+a = A.new 1
+b = A.new 10
+
 a.increment
 a.increment
 a.increment
-puts a.foo
+a.increment
+a.increment
+a.increment
+
+puts "A.global_increment: #{A.global_increment}"
+
+puts "a.local_increment: #{a.local_increment}"
+puts "a.local_increment: #{a.global_increment}"
+
+puts "b.local_increment: #{b.local_increment}"
+puts "b.local_increment: #{b.global_increment}"
