549e3cd5aac81e8ab15b1a5596b5dae9f03f57ea
Marcos Minond <minond.marcos@gmail.com>
2018-01-30 22:10:45 -0700

    interpreter.rkt

---
+++ Programming Languages/Module 07/interpreter.rkt
@@ -0,0 +1,40 @@
+#lang racket
+
+(provide (all-defined-out))
+
+(struct const (int) #:transparent)
+(struct negate (e) #:transparent)
+(struct add (e1 e2) #:transparent)
+(struct bool (b) #:transparent)
+(struct eq-num (e1 e2) #:transparent)
+(struct if-then-else (e1 e2 e3) #:transparent)
+
+(define (eval-exp e)
+  (cond [(const? e) e]
+        [(bool? e) e]
+
+        [(negate? e)
+         (let ([v (eval-exp (negate-e e))])
+           (if (const? v)
+             (const (- (const-int v)))
+             ((error "negate applied to non-number"))))]
+
+        [(add? e)
+         (let ([v1 (eval-exp (add-e1 e))]
+               [v2 (eval-exp (add-e2 e))])
+           (cond [(not (const? v1)) (error "non-number on left hand side of add expression")]
+                 [(not (const? v2)) (error "non-number on right hand side of add expression")]
+                 [#t (const (+ (const-int v1) (const-int v2)))]))]
+
+        [(eq-num? e)
+         (let ([v1 (eval-exp (eq-num-e1 e))]
+               [v2 (eval-exp (eq-num-e2 e))])
+           (if (and (const? v1) (const? v2))
+             (bool (= v1 v2))
+             (error "non-number applied to eq-num expression")))]
+
+        [(if-then-else? e)
+         (let ([check (eval-exp (if-then-else-e1 e))])
+           (cond [(and (bool? check) (bool-b check)) (eval-exp (if-then-else-e2 e))]
+                 [(bool? check) (eval-exp (if-then-else-e3 e))]
+                 [#t (error "non-bool in if-then-else condition")]))]))
