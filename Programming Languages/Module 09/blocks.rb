e638fbe4562dac67210512c4739f25d69273f9f3
Marcos Minond <minond.marcos@gmail.com>
2018-02-06 09:37:44 -0700

    blocks.rb

---
+++ Programming Languages/Module 09/blocks.rb
@@ -0,0 +1,13 @@
+foo = Array.new(5) { |i| i }
+bar = foo.map { |i| i * i }
+
+# Synonyms:
+# - map: collect
+# - filter: select
+# - reduce: inject
+
+p foo
+p bar
+p foo.any? { |i| i > 15 }
+p bar.any? { |i| i > 15 }
+p bar.inject(0) { |acc, x| acc + x }
