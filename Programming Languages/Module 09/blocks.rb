8951601639cdee1e434d851aaa004a570375f2e0
Marcos Minond <minond.marcos@gmail.com>
2018-02-06 09:41:02 -0700

    blocks.rb

--- Programming Languages/Module 09/blocks.rb
+++ Programming Languages/Module 09/blocks.rb
@@ -11,3 +11,18 @@ p bar
 p foo.any? { |i| i > 15 }
 p bar.any? { |i| i > 15 }
 p bar.inject(0) { |acc, x| acc + x }
+
+def t i
+  (0..i).each do |j|
+    print "  " * j
+    (j..i).each do |k|
+      print k
+      print " "
+    end
+
+    print "\n"
+  end
+end
+
+puts
+t 9
