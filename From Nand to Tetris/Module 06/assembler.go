bc69608acaa8c43ff1352f881dd06ffcf5de3137
Marcos Minond <minond.marcos@gmail.com>
2018-03-08 21:32:36 -0700

    id

--- From Nand to Tetris/Module 06/assembler.go
+++ From Nand to Tetris/Module 06/assembler.go
@@ -19,23 +19,22 @@ type token struct {
 }
 
 const (
-	andToken     tokenId = "and"
-	atToken      tokenId = "at"
-	bitToken     tokenId = "bit"
-	commentToken tokenId = "comment"
-	cparenToken  tokenId = "cparen"
-	eolToken     tokenId = "eol"
-	eqToken      tokenId = "eq"
-	errToken     tokenId = "err"
-	idToken      tokenId = "id"
-	minusToken   tokenId = "minus"
-	nlToken      tokenId = "nl"
-	notToken     tokenId = "not"
-	numToken     tokenId = "num"
-	oparenToken  tokenId = "oparen"
-	orToken      tokenId = "or"
-	plusToken    tokenId = "plus"
-	scolonToken  tokenId = "scolon"
+	andToken    tokenId = "and"
+	atToken     tokenId = "at"
+	bitToken    tokenId = "bit"
+	cparenToken tokenId = "cparen"
+	eolToken    tokenId = "eol"
+	eqToken     tokenId = "eq"
+	errToken    tokenId = "err"
+	idToken     tokenId = "id"
+	minusToken  tokenId = "minus"
+	nlToken     tokenId = "nl"
+	notToken    tokenId = "not"
+	numToken    tokenId = "num"
+	oparenToken tokenId = "oparen"
+	orToken     tokenId = "or"
+	plusToken   tokenId = "plus"
+	scolonToken tokenId = "scolon"
 )
 
 var (
@@ -57,12 +56,39 @@ var (
 )
 
 func main() {
-	source := `@2
-D=A
-@3
+	source := `
+
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/06/max/Max.asm
+
+// Computes R2 = max(R0, R1)  (R0,R1,R2 refer to RAM[0],RAM[1],RAM[2])
+
+   @R0
+   D=M              // D = first number
+   @R1
+   D=D-M            // D = first number - second number
+   @OUTPUT_FIRST
+   D;JGT            // if D>0 (first is greater) goto output_first
+   @R1
+   D=M              // D = second number
+   @OUTPUT_D
+   0;JMP            // goto output_d
+(OUTPUT_FIRST)
+   @R0
+   D=M              // D = first number
+(OUTPUT_D)
+   @R2
+   M=D              // M[2] = D (greatest number)
+(INFINITE_LOOP)
+   @INFINITE_LOOP
+   0;JMP            // infinite loop
 D=D+A
 @0
-M=D`
+
+
+`
 
 	fmt.Println(scan(source))
 }
@@ -74,11 +100,15 @@ func scan(source string) []token {
 
 func (s scanner) scan() []token {
 	var tokens []token
+	idfn := or(unicode.IsDigit, unicode.IsLetter, is(rune('_')))
 
 	for !s.done() {
 		curr := s.curr()
 
-		if id, ok := runeToks[curr]; ok {
+		if unicode.IsSpace(curr) {
+			s.consume()
+			continue
+		} else if id, ok := runeToks[curr]; ok {
 			lexeme := string(curr)
 
 			if id == nlToken {
@@ -92,7 +122,11 @@ func (s scanner) scan() []token {
 			})
 
 			s.consume()
-		} else if lexeme := s.takeWhile(unicode.IsLetter); len(lexeme) > 0 {
+		} else if curr == rune('/') && s.peek() == rune('/') {
+			s.takeUntil(func(r rune) bool {
+				return r == rune('\n')
+			})
+		} else if lexeme := s.takeWhile(idfn); len(lexeme) > 0 {
 			tokens = append(tokens, token{
 				pos:    s.pos,
 				id:     idToken,
@@ -148,4 +182,38 @@ func (s *scanner) takeWhile(f func(rune) bool) string {
 	}
 
 	return string(buff)
+}
+
+func (s *scanner) takeUntil(f func(rune) bool) string {
+	var buff []rune
+
+	for !s.done() {
+		if f(s.curr()) {
+			s.consume()
+			break
+		}
+
+		buff = append(buff, s.curr())
+		s.consume()
+	}
+
+	return string(buff)
+}
+
+func or(fs ...func(rune) bool) func(rune) bool {
+	return func(r rune) bool {
+		for _, f := range fs {
+			if f(r) {
+				return true
+			}
+		}
+
+		return false
+	}
+}
+
+func is(v rune) func(rune) bool {
+	return func(r rune) bool {
+		return r == v
+	}
 }
