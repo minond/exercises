72f09f846bcfb4662019cb21ca746b439e411570
Marcos Minond <minond.marcos@gmail.com>
2018-03-12 15:37:02 -0600

    Task2/task2.c

--- Embedded Hardware and Operating Systems/Networking/Task2/task2.c
+++ Embedded Hardware and Operating Systems/Networking/Task2/task2.c
@@ -23,8 +23,8 @@ struct neighbor_t {
   struct ctimer ttl;
 };
 
+static rimeaddr_t *get_rand_addr();
 static void drop(void *);
-
 static void save(struct announcement *, const rimeaddr_t *, uint16_t, uint16_t);
 
 static void recv(struct multihop_conn *, const rimeaddr_t *, const rimeaddr_t *,
@@ -45,8 +45,8 @@ AUTOSTART_PROCESSES(&task2);
 
 PROCESS_THREAD(task2, ev, data) {
   char buff[MSG_LEN];
+  rimeaddr_t *addr;
   static struct etimer timer;
-  rimeaddr_t to = {{1, 0}};
 
   PROCESS_EXITHANDLER(multihop_close(&multihop);)
   PROCESS_BEGIN();
@@ -56,13 +56,20 @@ PROCESS_THREAD(task2, ev, data) {
   announcement_set_value(&ann, 0);
 
   while (1) {
-    etimer_set(&timer, CLOCK_SECOND * 5 + random_rand());
+    etimer_set(&timer, (CLOCK_SECOND * 5 + random_rand()) % 30);
 
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
 
-    sprintf(buff, "NodeId[%d]", node_id);
-    packetbuf_copyfrom(buff, MSG_LEN);
-    multihop_send(&multihop, &to);
+    addr = get_rand_addr();
+
+    if (addr != NULL) {
+      printf("Sending a message to %d.%d\n", addr->u8[0], addr->u8[1]);
+      sprintf(buff, "NodeId[%d]", node_id);
+      packetbuf_copyfrom(buff, MSG_LEN);
+      multihop_send(&multihop, addr);
+    } else {
+      printf("Unable to get neighbor address, not sending message\n");
+    }
   }
 
   PROCESS_END();
@@ -71,7 +78,8 @@ PROCESS_THREAD(task2, ev, data) {
 static void drop(void *obj) {
   struct neighbor_t *neighbor = obj;
 
-  printf("Removing neighbor %d.%d\n", neighbor->addr.u8[0], neighbor->addr.u8[1]);
+  printf("Removing neighbor %d.%d\n", neighbor->addr.u8[0],
+         neighbor->addr.u8[1]);
   list_remove(neighbors, neighbor);
   memb_free(&neighbor_mem, neighbor);
 }
@@ -103,10 +111,23 @@ static void recv(struct multihop_conn *conn, const rimeaddr_t *from,
 static rimeaddr_t *frwd(struct multihop_conn *conn, const rimeaddr_t *orig,
                         const rimeaddr_t *dest, const rimeaddr_t *prev,
                         uint8_t hops) {
+  rimeaddr_t *addr = get_rand_addr();
+
   printf("Received multihop message %d.%d -> %d.%d -> %d.%d: '%s'\n",
          orig->u8[0], orig->u8[1], prev->u8[0], prev->u8[1], dest->u8[0],
          dest->u8[1], (char *)packetbuf_dataptr());
 
+  if (addr != NULL) {
+    printf("Forwarding message to %d.%d (hop #%d)\n", addr->u8[0], addr->u8[1],
+           packetbuf_attr(PACKETBUF_ATTR_HOPS));
+  } else {
+    printf("Unable to get a neighbor address, not forwarding message\n");
+  }
+
+  return addr;
+}
+
+static rimeaddr_t *get_rand_addr() {
   struct neighbor_t *neighbor;
   int len = list_length(neighbors);
   int nidx, i;
@@ -121,15 +142,8 @@ static rimeaddr_t *frwd(struct multihop_conn *conn, const rimeaddr_t *orig,
     }
 
     if (neighbor != NULL) {
-      printf("Forwarding message to %d.%d (hop #%d)\n", neighbor->addr.u8[0],
-             neighbor->addr.u8[1], packetbuf_attr(PACKETBUF_ATTR_HOPS));
-
       return &neighbor->addr;
-    } else {
-      printf("Failed to get neighbor from list, not forwarding message\n");
     }
-  } else {
-    printf("No neighbors, not forwarding message\n");
   }
 
   return NULL;
