2186ca48ed478ae441a52293c4196b5b62b64ad9
Marcos Minond <minond.marcos@gmail.com>
2018-03-14 23:15:04 -0600

    task3-3.c

--- Embedded Hardware and Operating Systems/Networking/Task3/task3-2.c
+++ Embedded Hardware and Operating Systems/Networking/Task3/task3-2.c
@@ -15,7 +15,6 @@
 
 #include <stdio.h>
 
-static short pows(short, short);
 static void recv(struct trickle_conn *);
 
 const static struct trickle_callbacks callbacks = {recv};
@@ -25,13 +24,10 @@ PROCESS(task3_2, "Simulate trickle (Rime) in Cooja (1)");
 AUTOSTART_PROCESSES(&task3_2);
 
 PROCESS_THREAD(task3_2, ev, data) {
-  short interval = pows(10, -14 * CLOCK_SECOND);
-
   PROCESS_EXITHANDLER(trickle_close(&trickle);)
   PROCESS_BEGIN();
 
-  printf("Trickle every %i seconds\n", interval);
-  trickle_open(&trickle, CLOCK_SECOND, interval, &callbacks);
+  trickle_open(&trickle, 2*CLOCK_SECOND, 145, &callbacks);
   SENSORS_ACTIVATE(button_sensor);
 
   while (1) {
@@ -46,18 +42,4 @@ PROCESS_THREAD(task3_2, ev, data) {
 
 static void recv(struct trickle_conn *conn) {
   printf("Trickle message received '%s'\n", (char *)packetbuf_dataptr());
-}
-
-static short pows(short n, short x) {
-  if (x < 0) {
-    return pows(1 / x, -x);
-  } else if (x == 0) {
-    return 1;
-  } else if (x == 1) {
-    return n;
-  } else if (x % 2 == 0) {
-    return pows(n * n, x / 2);
-  } else {
-    return n * pows(n * n, (x - 1) / 2);
-  }
 }
