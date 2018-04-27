9239766fdc3eab0e7756758863a4269fced20471
Marcos Minond <minond.marcos@gmail.com>
2018-04-26 18:15:56 -0600

    ch8 wip

--- The Little Schemer/08-lambda-the-ultimate.scm
+++ The Little Schemer/08-lambda-the-ultimate.scm
@@ -1,5 +1,52 @@
 (load "00-common.scm")
 
+(define value
+  (lambda (exp)
+    (cond
+      ((atom? exp) exp)
+      ((eq? (car (cdr exp)) '+)
+       (+ (value (car exp))
+          (value (car (cdr (cdr exp))))))
+      ((eq? (car (cdr exp)) '^)
+       (expt (value (car exp))
+             (value (car (cdr (cdr exp))))))
+      ((eq? (car (cdr exp)) '*)
+       (* (value (car exp))
+          (value (car (cdr (cdr exp)))))))))
+
+(debug `(value '1))
+(debug `(value '(2 + 20)))
+(debug `(value '(3 * (21 + 1))))
+(debug `(value '(3 ^ 2)))
+
+
+(define multirember
+  (lambda (x xs)
+    (cond
+      ((null? xs) '())
+      ((eq? x (car xs))
+       (multirember x (cdr xs)))
+      (#t (cons (car xs)
+                (multirember x (cdr xs)))))))
+
+(define multirember-f
+  (lambda (test?)
+    (lambda (x xs)
+      (cond
+        ((null? xs) '())
+        ((test? x (car xs))
+         ((multirember-f test?) x (cdr xs)))
+        (#t (cons (car xs)
+                  ((multirember-f test?) x (cdr xs))))))))
+
+(define multirember-eq?
+  (multirember-f eq?))
+
+(debug `(multirember 2 '(1 2 3 4 5)))
+(debug `(multirember-eq? 2 '(1 2 3 4 5)))
+(debug `((multirember-f eq?) 2 '(1 2 3 4 5)))
+
+
 (define eq?-c
   (lambda (a)
     (lambda (b)
