51e5e16d658ab400ab7c46166a940ae65f80bd9b
Marcos Minond <minond.marcos@gmail.com>
2018-05-22 00:07:53 -0600

    Y

--- The Little Schemer/09-and-again.scm
+++ The Little Schemer/09-and-again.scm
@@ -46,3 +46,54 @@
      (cond
        ((null? l) 0)
        (else (add1 (length (cdr l))))))))
+
+(define (eternity)
+  (eternity))
+
+(((lambda (mk-length)
+    (mk-length mk-length))
+  (lambda (mk-length)
+    (lambda (l)
+      (cond
+        ((null? l) 0)
+        (else (add1 ((mk-length mk-length)
+                     (cdr l))))))))
+ '(apples 1 2 3 4 5 6))
+
+(((lambda (mk-length)
+    (mk-length mk-length))
+  (lambda (mk-length)
+    ((lambda (length)
+       (lambda (l)
+         (cond
+           ((null? l) 0)
+           (else (add1 (length (cdr l)))))))
+     (lambda (x)
+       ((mk-length mk-length) x)))))
+ '(apples 1 2 3 4 5 6 7 8 9))
+
+(((lambda (le)
+    ((lambda (mk-length)
+       (mk-length mk-length))
+     (lambda (mk-length)
+       (le (lambda (x)
+             ((mk-length mk-length) x))))))
+  (lambda (length)
+    (lambda (l)
+      (cond
+        ((null? l) 0)
+        (else (add1 (length (cdr l))))))))
+ '(apples 1 2 3 4 5 6 7 8 9))
+
+(define Y
+  (lambda (le)
+    ((lambda (f) (f f))
+     (lambda (f)
+       (le (lambda (x) ((f f) x)))))))
+
+(define my-length (Y
+                    (lambda (length)
+                      (lambda (l)
+                        (cond
+                          ((null? l) 0)
+                          (else (add1 (length (cdr l)))))))))
