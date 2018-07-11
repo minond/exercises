288e2d6311fe5753f997b52439353dbad67c1f39
Marcos Minond <minond.marcos@gmail.com>
2018-07-11 15:36:38 -0600

    test

--- Racket School 2018/work/tacogram/grammar.rkt
+++ Racket School 2018/work/tacogram/grammar.rkt
@@ -2,7 +2,7 @@
 
 taco-program : taco-leaf+
 
-taco-leaf : /ws{0,} /open [taco | not-a-taco]{7} /close /ws{0,}
+taco-leaf : /ws{0,} /open (taco | not-a-taco){7} /close /ws{0,}
 
 taco : /"%"
 not-a-taco : /"#" /"$"
