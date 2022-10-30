b3dc0a7a36acefdf0d35cfe5ee9f61cb2e8160b2
Marcos Minond <minond.marcos@gmail.com>
2022-10-30 16:52:16 -0600

    Just printing classes

--- vm/jvm/main.go
+++ vm/jvm/main.go
@@ -24,19 +24,28 @@ func main() {
 		fmt.Printf("%s:\n", class.Name.Value)
 		for _, method := range class.Methods {
 			fmt.Printf("  %s:\n", method.Name.Value)
+			// pretty.Println(method)
 			method.PrintInstructions()
 		}
 	}
 
-	fmt.Println("-----------------")
-	for _, method := range cf.Methods {
-		name, ok := cf.ConstantPool[method.NameIndex-1].(*Utf8Info)
-		if !ok {
-			panic("...")
-		}
+	// fmt.Println("-----------------")
+	// for _, method := range cf.Methods {
+	// 	name, ok := cf.ConstantPool[method.NameIndex-1].(*Utf8Info)
+	// 	if !ok {
+	// 		panic("...")
+	// 	}
+	//
+	// 	m := Method{Name: name, Impl: []*MethodInfo{method}}
+	// 	fmt.Printf("  %s:\n", m.Name.Value)
+	// 	m.PrintInstructions()
+	// }
 
-		m := Method{Name: name, Impl: []*MethodInfo{method}}
-		fmt.Printf("  %s:\n", m.Name.Value)
-		m.PrintInstructions()
-	}
+	// fmt.Printf("getstatic #20: %s\n", pretty.Sprint(cf.ConstantPool[20-1]))
+	// fmt.Printf("  | class index: %s\n", pretty.Sprint(cf.ConstantPool[0x15-1]))
+	// fmt.Printf("    | name: %s\n", pretty.Sprint(cf.ConstantPool[0x17-1]))
+	// fmt.Printf("  | name and type index: %s\n", pretty.Sprint(cf.ConstantPool[0x16-1]))
+	// fmt.Printf("    | name: %s\n", pretty.Sprint(cf.ConstantPool[0x18-1]))
+
+	// pretty.Println(cf.Methods)
 }
